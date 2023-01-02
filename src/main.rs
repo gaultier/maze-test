b8f7a50f7d13458d22064e4aba6ba676f028b54c
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-02 12:55:08 +0100

    [wip] bfs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
-use rusqlite::{Connection, Result};
+//use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
-use std::convert::TryFrom;
+use std::{collections::VecDeque, convert::TryFrom};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct CreateMazeHttpRequest {
@@ -11,13 +11,24 @@ struct CreateMazeHttpRequest {
     walls: Vec<String>,
 }
 
-#[derive(Debug)]
-struct MazeCellCoord {
-    column: u8,
-    row: u8,
-}
+type Position = usize;
 
 #[derive(Debug)]
+struct Coord(usize, usize);
+
+impl Coord {
+    fn to_pos(self: &Self, width: usize) -> usize {
+        return self.1 * width + self.0;
+    }
+
+    fn from_pos(pos: usize, width: usize) -> Self {
+        let x = pos % width;
+        let y = pos / width;
+        return Coord(x, y);
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum MazeCellKind {
     Wall,
     Empty,
@@ -27,12 +38,18 @@ enum MazeCellKind {
 
 #[derive(Debug)]
 struct CreateMazeRequest {
-    entrance: MazeCellCoord,
+    entrance: Coord,
     grid_size: (u8, u8),
-    walls: Vec<MazeCellCoord>,
+    walls: Vec<Coord>,
 }
 
-impl TryFrom<&str> for MazeCellCoord {
+#[derive(Debug)]
+struct Node {
+    maze_pos: usize,
+    parent_idx: Option<usize>,
+}
+
+impl TryFrom<&str> for Coord {
     type Error = Error;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
@@ -73,10 +90,9 @@ impl TryFrom<&str> for MazeCellCoord {
             });
         }
 
-        Ok(MazeCellCoord {
-            column: char_bytes[0] - 65,
-            row: row - 1,
-        })
+        let column = char_bytes[0] - 65;
+
+        Ok(Coord(row as usize, column as usize))
     }
 }
 
@@ -124,6 +140,59 @@ struct Error {
     error: String,
 }
 
+fn bfs(maze: &[MazeCellKind], entrance: Coord, width: usize, height: usize) {
+    let mut explored = Vec::with_capacity(width * height);
+    explored.resize(explored.capacity(), false);
+
+    let mut work: VecDeque<Node> = VecDeque::new();
+    work.push_front(Node {
+        maze_pos: entrance.to_pos(width),
+        parent_idx: None,
+    });
+
+    let entrance_pos = entrance.to_pos(width);
+    explored[entrance_pos] = true;
+
+    while !work.is_empty() {
+        let w = work.pop_front().unwrap();
+        let work_pos = w.maze_pos;
+
+        if maze[work_pos] == MazeCellKind::Exit {
+            return;
+        }
+
+        let work_coord = Coord::from_pos(work_pos, width);
+        let adjacent_cells: [(isize, isize); 4] = [
+            (work_coord.0 as isize + 1, work_coord.1 as isize),
+            (work_coord.0 as isize - 1, work_coord.1 as isize),
+            (work_coord.0 as isize, work_coord.1 as isize + 1),
+            (work_coord.0 as isize, work_coord.1 as isize - 1),
+        ];
+
+        for adjacent_cell in adjacent_cells {
+            // Out of bounds
+            if adjacent_cell.0 < 0
+                || adjacent_cell.1 < 0
+                || adjacent_cell.0 as usize >= width
+                || adjacent_cell.1 as usize >= height
+            {
+                continue;
+            }
+
+            let adjacent_cell_pos =
+                Coord(adjacent_cell.0 as usize, adjacent_cell.1 as usize).to_pos(width);
+
+            if !explored[adjacent_cell_pos] {
+                explored[adjacent_cell_pos] = true;
+                work.push_back(Node {
+                    maze_pos: adjacent_cell_pos,
+                    parent_idx: Some(adjacent_cell_pos),
+                });
+            }
+        }
+    }
+}
+
 async fn create_maze(create_maze_http_req: web::Json<CreateMazeHttpRequest>) -> HttpResponse {
     let create_maze_req: CreateMazeRequest = match (&create_maze_http_req).try_into() {
         Err(err) => {
@@ -137,10 +206,10 @@ async fn create_maze(create_maze_http_req: web::Json<CreateMazeHttpRequest>) -> 
     let width = create_maze_req.grid_size.0 as usize;
     let height = create_maze_req.grid_size.1 as usize;
     let mut maze: Vec<MazeCellKind> = Vec::with_capacity(width * height);
-    maze.resize_with(maze.capacity(), || MazeCellKind::Empty);
+    maze.resize(maze.capacity(), MazeCellKind::Empty);
 
     for cell in create_maze_req.walls {
-        let pos = cell.column as usize * width + cell.row as usize;
+        let pos = cell.to_pos(width);
         maze[pos] = MazeCellKind::Wall;
     }
 
@@ -155,8 +224,7 @@ async fn create_maze(create_maze_http_req: web::Json<CreateMazeHttpRequest>) -> 
         maze[pos] = MazeCellKind::Exit;
     }
 
-    let entrance_pos =
-        create_maze_req.entrance.column as usize * width + create_maze_req.entrance.row as usize;
+    let entrance_pos = create_maze_req.entrance.to_pos(width);
     maze[entrance_pos] = MazeCellKind::Entry;
     println!("{:?}", maze);
 
@@ -172,8 +240,6 @@ async fn create_maze(create_maze_http_req: web::Json<CreateMazeHttpRequest>) -> 
         }
         println!("");
     }
-
-    //for cell in create_maze_req.
 
     //    let conn = match Connection::open_in_memory() {
     //        Ok(conn) => conn,
